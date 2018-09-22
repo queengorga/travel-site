0f82157cd5334abee02d7c124bacf0366e635090
Victoria Allen <vickmae.allen@gmail.com>
2018-09-22 08:29:24 -0700

    Attractions section modification; added slideshow

---
+++ script.js
@@ -0,0 +1,22 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var pslides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > pslides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = pslides.length }
+    for (i = 0; i < pslides.length; i++) {
+        pslides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    pslides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
